223bb8a69dd60bd70bae7acd2eb0491a055110d4
Douglas Schilling Landgraf <dougsland@redhat.com>
2011-08-15 22:33:53 -0300

    Adding .tar.gz and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='python-confparser', version='1.0.0',
+      description='Python Nagios extension to RHEV',
+      author='Douglas Schilling Landgraf',
+      author_email='dougsland@redhat.com',
+      url='https://github.com/dougsland/nagios-plugins-rhev/wiki',
+      classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: System Administrators',
+          'License :: GPLv2+',
+          'Programming Language :: Python',
+          ],
+      license= 'GPLv2+' ,
+)
